291782b5922a072139205a247b5b0326093821a2
inmeriel25 <inmeriel@gmail.com>
2024-12-10 09:54:57 +0900

    first commit

---
+++ data/todos.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        "id":1,
+        "title": "청소하기",
+        "completed": true
+    },
+    {
+        "id":2,
+        "title": "요플레먹기",
+        "completed": true
+    },
+    {
+        "id":3,
+        "title": "헬스장가서 웨이트 하기",
+        "completed": false
+    },
+    {
+        "id":4,
+        "title": "러닝 515 페이스 유지하기",
+        "completed": true
+    },
+]
